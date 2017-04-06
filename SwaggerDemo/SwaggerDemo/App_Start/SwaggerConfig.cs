b0a59d1398d18e7d42c258a26d76716d7f83b598
rkenavey <ryan@olo.com>
2017-04-06 17:29:32 -0400

    XML Comments

--- SwaggerDemo/SwaggerDemo/App_Start/SwaggerConfig.cs
+++ SwaggerDemo/SwaggerDemo/App_Start/SwaggerConfig.cs
@@ -1,9 +1,10 @@
+using System.IO;
+using System.Web;
 using System.Web.Http;
-using WebActivatorEx;
 using SwaggerDemo;
 using Swashbuckle.Application;
 
-[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
 namespace SwaggerDemo
 {
@@ -97,7 +98,7 @@ namespace SwaggerDemo
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -241,6 +242,11 @@ namespace SwaggerDemo
                         //
                         //c.EnableApiKeySupport("apiKey", "header");
                     });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return Path.Combine(HttpRuntime.AppDomainAppPath, "bin", "SwaggerDemo.XML");
         }
     }
 }
